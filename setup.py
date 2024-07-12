47fe608fdc4194142ac5edd39d9d503237ff468d
XpliSyL <syl_element@hotmail.com>
2024-07-12 11:25:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,12 @@ setup(
     version="2.6.1",
     python_requires=">=3.6",
     packages=["autocorrect"],
-    package_data={"autocorrect": ["data/*"]},
+    package_data={"autocorrect": [
+        "data/fr.tar.gz",
+        "data/de.tar.gz",
+        "data/en.tar.gz",
+        "data/it.tar.gz"
+    ]},
     description="Spelling Corrector",
     author="Jonas McCallum, Filip Sondej",
     author_email="filipsondej@protonmail.com",
